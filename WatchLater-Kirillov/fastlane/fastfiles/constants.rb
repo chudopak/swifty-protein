c9e189aba9459b1c3b5db448cb7c9439d822556e
Rustem <47086556+ironrustem@users.noreply.github.com>
2022-04-15 12:16:37 +0300

    [TECH]  fix pipelines

--- WatchLater-Kirillov/fastlane/fastfiles/constants.rb
+++ WatchLater-Kirillov/fastlane/fastfiles/constants.rb
@@ -11,9 +11,9 @@ GIT_DISTRIBUTION_URL = "git@git.technokratos.com:ios/match-distribution.git"
 TEMP_BUILD_FOLDER = "#{File.dirname(Dir.pwd)}/"
 COMMON_CONFIG_FILE_PATH = TEMP_BUILD_FOLDER + "Configurations/Project.xcconfig"
 
-PROD_SCHEME = "WatchLater_Prod"
-DEV_SCHEME = "WatchLater_Dev"
-WORKSPACE = "WatchLater"
+PROD_SCHEME = "WatchLater-Kirillov_Prod"
+DEV_SCHEME = "WatchLater-Kirillov_Dev"
+WORKSPACE = "WatchLater-Kirillov"
 
 def app_short_version
   `awk '{if ($1 ~ /PRODUCT_VERSION/) {print $3}}' #{COMMON_CONFIG_FILE_PATH}`.strip
